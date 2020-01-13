1aa7680f088e9a4ed77832b9e9e1fb095dea24df
alternativeFlow <danxue@gmail.com>
2020-01-13 10:43:19 -0800

    Updates

---
+++ precache-manifest.7e4ea886c2f26f36d264a788fef6f4e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e72d0f6b920c7d2af59244de1ac054",
+    "url": "/websiteXueBob/index.html"
+  },
+  {
+    "revision": "18b497b69ffcf9f52e7f",
+    "url": "/websiteXueBob/static/css/main.bdfcddb0.chunk.css"
+  },
+  {
+    "revision": "142aee3395cb53bb9182",
+    "url": "/websiteXueBob/static/js/2.ea68d62a.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/websiteXueBob/static/js/2.ea68d62a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18b497b69ffcf9f52e7f",
+    "url": "/websiteXueBob/static/js/main.49175d30.chunk.js"
+  },
+  {
+    "revision": "29b168d7365073b2fe31",
+    "url": "/websiteXueBob/static/js/runtime-main.3de82374.js"
+  },
+  {
+    "revision": "d0fa511017e3609707a27cf7cc5212a6",
+    "url": "/websiteXueBob/static/media/beakers.d0fa5110.jpg"
+  },
+  {
+    "revision": "5f7c3921aaf2dcfc016cf015a6624fae",
+    "url": "/websiteXueBob/static/media/green-leaf.5f7c3921.jpg"
+  },
+  {
+    "revision": "4f0b534b318d9810b439a0393f074085",
+    "url": "/websiteXueBob/static/media/lab-plant.4f0b534b.jpg"
+  },
+  {
+    "revision": "5d30e6b4ce10b3135d6f74019fcea412",
+    "url": "/websiteXueBob/static/media/microscope.5d30e6b4.jpg"
+  },
+  {
+    "revision": "f3a1874caa2a2eb7327d7f4c611f9197",
+    "url": "/websiteXueBob/static/media/woodCover.f3a1874c.jpg"
+  }
+]);
